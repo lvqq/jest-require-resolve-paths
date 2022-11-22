2fc5ff73e0cf35ca5f808bd044504e5b85ba776e
lvqq <nicksonlvqq@gmail.com>
2022-11-22 10:29:02 +0800

    test: add different cases

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const paths = require.resolve.paths('npm')
-
-console.log('paths: ', paths);
+const modulePaths = require.resolve.paths('npm')
+const relativePaths = require.resolve.paths('./test.js')
+const builtinPaths = require.resolve.paths('fs')
+console.log('modulePaths: ', modulePaths);
+console.log('relativePaths: ', relativePaths);
+console.log('builtinPaths: ', builtinPaths);
