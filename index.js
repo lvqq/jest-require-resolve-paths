d6e494a74d51e49d085a31fa78a7f10bb74be8e0
lvqq <nicksonlvqq@gmail.com>
2022-11-24 22:04:57 +0800

    test: add absolute paths

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const modulePaths = require.resolve.paths('npm')
-const relativePaths = require.resolve.paths('./test.js')
+const absolutePaths = require.resolve.paths('/')
+const relativePaths = require.resolve.paths('./')
 const builtinPaths = require.resolve.paths('fs')
 console.log('modulePaths: ', modulePaths);
+console.log('absolutePaths: ', absolutePaths);
 console.log('relativePaths: ', relativePaths);
 console.log('builtinPaths: ', builtinPaths);
